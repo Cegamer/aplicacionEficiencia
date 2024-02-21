e77e0ece3b428db9c32221e29f5ee001c2f50189
Mateo Cerquera Olarte <mateoc09+2@gmail.com>
2024-02-20 23:21:11 -0500

    Mejora en el diseño del selector de app y funcionalidad

---
+++ AplicacionEficiencia/Core/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AplicacionEficiencia.Core
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _handler;
+
+        public RelayCommand(Action<object> handler)
+        {
+            _handler = handler;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter) => _handler(parameter);
+    }
+}
